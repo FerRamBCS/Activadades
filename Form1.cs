89028f9cdcd1879e3497b6922f7702a0e80f9c51
FerRamBCS <jfer1302@gmail.com>
2024-09-18 08:04:26 -0700

    prueba

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,16 @@ namespace prueba1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Console.WriteLine("prueba");
+            var num1 = 10;
+            var num2 = 20;
+            int resultado = Suma (num1, num2)
+        }
+
+        private int Suma(int a, int b)
+        { 
+            int totalSuma = a + b;
+
+            return totalSuma;
         }
     }
 }
